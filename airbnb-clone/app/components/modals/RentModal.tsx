8b9cf6bf3872bbb45144c5b32b6f20389d0e8129
Aakash Mukherjee <110044436+blackcode1996@users.noreply.github.com>
2023-04-13 11:53:46 +0530

    location

--- airbnb-clone/app/components/modals/RentModal.tsx
+++ airbnb-clone/app/components/modals/RentModal.tsx
@@ -6,6 +6,9 @@ import Heading from '../Heading'
 import { categories } from '../navbar/Categories'
 import CategoryInput from '../inputs/CategoryInput'
 import { FieldValues, useForm } from 'react-hook-form'
+import CountrySelect from '../inputs/CountrySelect'
+import dynamic from 'next/dynamic'
+
 
 enum STEPS{
   CATEGORY=0,
@@ -46,6 +49,11 @@ const RentModal = () => {
   })
 
   const category=watch("category")
+  const location=watch("location")
+
+  const Map=useMemo(()=>dynamic(()=>import ("../Map"),{
+    ssr:false
+  }),[location])
 
   const setCustomValue=(id:string,value:any)=>{
     setValue(id,value,{
@@ -101,8 +109,16 @@ const RentModal = () => {
 
   if(step===STEPS.LOCATION){
     bodyContent=(
-      <div>
-        Location Step!
+      <div className='flex flex-col gap-8'>
+        <Heading 
+        title='Where is your place located?'
+        subtitle='Help guests find you!'
+        />
+        <CountrySelect
+          value={location}
+          onChange={(value)=>setCustomValue('location',value)}
+        />
+      <Map center={location?.latlng}/>
       </div>
     )
   }
