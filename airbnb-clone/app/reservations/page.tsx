ee3687bf2e6673a010e997acb2db24798a388cbe
Bipin <biping579@gmail.com>
2023-04-17 18:32:21 +0530

    error 3 solved

--- airbnb-clone/app/reservations/page.tsx
+++ airbnb-clone/app/reservations/page.tsx
@@ -1,33 +1,44 @@
-import EmptyState from "../components/EmptyState";
+
+import EmptyState from "@/app/components/EmptyState";
 
 import getCurrentUser from "../actions/getCurrentuser";
 import getReservations from "../actions/getResevations";
-import ReservationsCLient from "./ReservationsCLient";
 
-const ReservationPage = async () => {
+import TripsClient from "../trips/TripsClient";
+
+const ReservationsPage = async () => {
   const currentUser = await getCurrentUser();
 
   if (!currentUser) {
-    return <EmptyState title="Unauthorized" subtitle="Please login" />;
+    return (
+      
+        <EmptyState
+          title="Unauthorized"
+          subtitle="Please login"
+        />
+      
+    )
   }
-  const reservations = await getReservations({
-    authorId: currentUser.id,
-  });
+
+  const reservations = await getReservations({ authorId: currentUser.id });
 
   if (reservations.length === 0) {
     return (
-      <EmptyState
-        title="No reservations found"
-        subtitle="looks like you have no reservations on your properties"
-      />
+        <EmptyState
+          title="No reservations found"
+          subtitle="Looks like you have no reservations on your properties."
+        />
+      
     );
-    }
-    
-    return <ReservationsCLient
+  }
+
+  return (
+      <TripsClient
         reservations={reservations}
-        currentuser={currentUser}
-
-    />
-};
-
-export default ReservationPage;
+        currentUser={currentUser}
+      />
+    
+  );
+}
+ 
+export default ReservationsPage;
