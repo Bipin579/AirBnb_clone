7d785a6aa740bf042a0f568a20a9af1c44468f4c
Bipin <biping579@gmail.com>
2023-04-18 14:54:27 +0530

    error solved

--- airbnb-clone/app/components/inputs/CountrySelect.tsx
+++ airbnb-clone/app/components/inputs/CountrySelect.tsx
@@ -1,65 +1,60 @@
-'use client';
+"use client";
 
-import Select from 'react-select'
+import useCountries from "@/app/hooks/useCountries";
+import Flag from "react-world-flags";
+import Select from "react-select";
 
-import useCountries from '@/app/hooks/useCountries';
 
 export type CountrySelectValue = {
   flag: string;
   label: string;
-  latlng: number[],
+  latlng: number[];
   region: string;
-  value: string
-}
+  value: string;
+};
 
-interface CountrySelectProps {
+type Props = {
   value?: CountrySelectValue;
   onChange: (value: CountrySelectValue) => void;
-}
+};
 
-const CountrySelect: React.FC<CountrySelectProps> = ({
-  value,
-  onChange
-}) => {
+function CountrySelect({ value, onChange }: Props) {
   const { getAll } = useCountries();
 
-  return ( 
+  return (
     <div>
       <Select
         placeholder="Anywhere"
         isClearable
         options={getAll()}
         value={value}
-        onChange={(value) => onChange(value as CountrySelectValue)}
+        onChange={(value:any) => onChange(value as CountrySelectValue)}
         formatOptionLabel={(option: any) => (
-          <div className="
-          flex flex-row items-center gap-3">
-            <div>{option.flag}</div>
+          <div className="flex flex-row items-center gap-3">
+            <Flag code={option.value} className="w-5" />
             <div>
               {option.label},
-              <span className="text-neutral-500 ml-1">
-                {option.region}
-              </span>
+              <span className="text-neutral-500 ml-1">{option.region}</span>
             </div>
           </div>
         )}
         classNames={{
-          control: () => 'p-3 border-2',
-          input: () => 'text-lg',
-          option: () => 'text-lg'
+          control: () => "p-3 border-2",
+          input: () => "text-lg",
+          option: () => "text-lg",
         }}
-        theme={(theme) => ({
+        theme={(theme:any) => ({
           ...theme,
           borderRadius: 6,
           colors: {
             ...theme.colors,
-            primary: 'black',
-            primary25: '#ffe4e6'
-          }
+            primary: "black",
+            primary25: "#ffe4e6",
+          },
         })}
       />
     </div>
-   );
+  );
 }
- 
+
 export default CountrySelect;
