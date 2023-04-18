60006a241a4cdc59983750019e8948c57eceba27
Bipin <biping579@gmail.com>
2023-04-18 11:05:08 +0530

    error_resolved1.3

--- airbnb-clone/app/page.tsx
+++ airbnb-clone/app/page.tsx
@@ -1,52 +1,54 @@
+import Container from "@/app/components/Container";
+import ListingCard from "@/app/components/listings/ListingCard";
+import EmptyState from "@/app/components/EmptyState";
 
+import getListings, { IListingsParams } from "@/app/actions/getListings";
 import getCurrentUser from "./actions/getCurrentuser";
-import getListings, { IListingsParams } from "./actions/getListings";
-import Container from "./components/Container";
-import EmptyState from "./components/EmptyState";
-import ListingCard from "./components/listings/ListingCard";
+import ClientOnly from "./components/ClientOnly";
 
-interface HomeProps{
-  searchParams:IListingsParams
+interface HomeProps {
+  searchParams: IListingsParams;
 }
 
-const Home=async ({searchParams}:HomeProps)=> {
-
-  const currentUser = await getCurrentUser();
+const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
-  if (listings.length===0) {
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
     return (
-      <EmptyState showReset />
-    )
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
   }
 
-
   return (
-    <Container>
-      <div
-        className="pt-24 
-       grid
-       grid-cols-1
-        sm:grid-cols-2
-        md:grid-cols-3
-        lg:grid-cols-4
-        xl:grid-cols-5
-        2xl-grid-cols-6
-        gap-8
-        "
-      >
-        {listings.map((listing) => {
-          return (
+    <ClientOnly>
+      <Container>
+        <div
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => (
             <ListingCard
               currentUser={currentUser}
               key={listing.id}
               data={listing}
             />
-  
-          )
-        })}
-      </div>
-    </Container>
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
   );
-}
+};
 
 export default Home;
